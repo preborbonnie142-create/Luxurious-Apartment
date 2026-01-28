846762da96ca844b683bedf45a38f96e5fc5d78b
Prebor Bonnie <preborbonnie@gmail.com>
2026-01-28 14:55:04 +0100

    serve frontend files

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
 const app =  express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(Path2D.join(__dirname, "frontend")));
 
 // Test route
 app.get("/", (req, res) => {
     res.send("Backend is running...");
+    res.sendFile(Path2D.join(__dirname, "frontend", "index.html"));
 });
 
 // Simple API
